e912306a6e4860cacc8913795f3e2605657e7947
Suresh Reddy Guntaka <sureshreddy.guntaka@gmail.com>
2017-04-24 10:48:42 +0530

    Strated implementing gulp tasks

--- layout/src/layout-module.js
+++ layout/src/layout-module.js
@@ -2,9 +2,6 @@
  * UI Layout Module
  */
 angular.module("ui.layout", ["ui.core", "ui.navigation"])
-    .config(function (FileLoaderProvider) {
-        console.log(FileLoaderProvider);
-    })
     .run(function () {
         console.log('Invoking Run of Layout Module');
     });
